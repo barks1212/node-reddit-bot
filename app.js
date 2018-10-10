cc5434c1bebdb46847ff25220a8f3e377f17a048
Will Barker <willbarkerwork@gmail.com>
2018-10-10 12:07:18 +0100

    basic stream of comments

---
+++ app.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const Snoowrap = require("snoowrap");
+const Snoostorm = require("snoostorm");
+
+// snoowrap and snoostorm clients
+const snoowrapConfig = new Snoowrap({
+  userAgent: "reddit-bot-example-node",
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  username: process.env.REDDIT_USER,
+  password: process.env.REDDIT_PASS
+});
+const client = new Snoostorm(snoowrapConfig);
+
+// configure options for stream
+const streamOpts = {
+  subreddit: "all",
+  results: 25
+};
+
+// create snoostorm comment stream
+const comments = client.CommentStream(streamOpts);
+
+// on comment, perform logic
+comments.on("comment", comment => {
+  console.log(comment);
+});
